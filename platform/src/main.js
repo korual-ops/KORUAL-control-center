788d626e18f310d6e0ca43d1077d743d9205b6e6
korual-ops <kys94607712@gmail.com>
2026-09-19 22:37:45 +0900

    fix: make Vite entry valid JavaScript

--- platform/src/main.js
+++ platform/src/main.js
@@ -4,5 +4,5 @@ import App from './app.jsx';
 import './style.css';
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  React.createElement(React.StrictMode, null, React.createElement(App))
 );
